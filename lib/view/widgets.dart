1ec4a52d01cd3f8116c254aea836a5eda8d70968
Adhitocho <Adhitocho@gmail.com>
2021-09-16 20:24:47 +0700

    update kedua malam

--- lib/view/widgets.dart
+++ lib/view/widgets.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 
 class WidgetsPage extends StatelessWidget {
-  final TextStyle robotoShadow =
-      TextStyle(fontFamily: 'Roboto', shadows: <Shadow>[
-    Shadow(
-      offset: Offset(1.0, 1.0),
-      blurRadius: 3.0,
-      color: Colors.grey,
-    ),
-  ]);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Text('test'),
+    );
+  }
+}
+
+final TextStyle menu =
+    TextStyle(fontFamily: 'Roboto', fontSize: 25, color: Colors.black);
+
+final TextStyle subMenu =
+    TextStyle(fontFamily: 'Roboto', fontSize: 17, color: Colors.black);
+
+class WidgetsPageSFul extends StatefulWidget {
+  const WidgetsPageSFul({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetsPageSFul> createState() => _WidgetsPageSFulState();
+}
+
+class _WidgetsPageSFulState extends State<WidgetsPageSFul> {
+  int _counter = 0;
+
+  increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  decrement() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 70,
+        height: 30,
+        child: Row(
+          children: [
+            FloatingActionButton(onPressed: increment(), child: Icon(Icons.add)), // tambah
+            Text('$_counter', style: subMenu),
+            FloatingActionButton(onPressed: decrement(), child: Icon(Icons.remove)), // kurang
+          ],
+        ),
+      ),
     );
   }
 }
